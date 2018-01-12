b1a4b32dd2b993f293e63b1d05902bede1737be8
Jordan Dobrev <jordan.dobrev@livebuzz.co.uk>
2018-01-12 16:13:28 +0000

    sort out UUID

--- src/Laravel/Eloquent/Traits/Uuid.php
+++ src/Laravel/Eloquent/Traits/Uuid.php
@@ -19,7 +19,7 @@ trait Uuid
         static::creating(
             function ($model) {
                 if (!$model->id) {
-                    $model->id = $model->generateUuid();
+                    $model->id = $model->generateUniqueUuid();
                 }
             }
         );
@@ -35,14 +35,12 @@ trait Uuid
     }
 
     /**
-     * @return bool|string
+     * @return string
      */
-    public function generateUuid()
+    public function generateUuid(): string
     {
-        $uuid = false;
-
-        while (true) {
-            $uuid = sprintf(
+        if (config()->has('replication.node')) {
+            return sprintf(
                 '%04x%04x-%04x-%04x-%04x-%04x%04x%04s',
                 // 32 bits for "time_low"
                 mt_rand(0, 0xffff),
@@ -61,6 +59,38 @@ trait Uuid
                 mt_rand(0, 0xffff),
                 strtolower(config('replication.node')) //actual node name from env config
             );
+        } else {
+            return sprintf(
+                '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                // 32 bits for "time_low"
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                // 16 bits for "time_mid"
+                mt_rand(0, 0xffff),
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 4
+                mt_rand(0, 0x0fff) | 0x4000,
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                mt_rand(0, 0x3fff) | 0x8000,
+                // 48 bits for "node"
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff) //actual node name from env config
+            );
+        }
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function generateUniqueUuid()
+    {
+        $uuid = false;
+
+        while (true) {
+            $uuid = $this->generateUuid();
 
             $exists = (new static)
                 ->newQueryWithoutScopes()
